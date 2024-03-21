04a9444738275945985c3935673b191893f01050
Hexalgo <hexalgo@gmail.com>
2024-03-21 17:39:20 +0100

    little fix on app.py

--- app/app.py
+++ app/app.py
@@ -6,22 +6,27 @@ from flask import Flask, jsonify, request, render_template
 from flask_socketio import SocketIO
 from pyd2bot.logic.managers.AccountManager import AccountManager
 from pyd2bot.Pyd2Bot import Pyd2Bot
-from pyd2bot.thriftServer.pyd2botService.ttypes import (Certificate, JobFilter,
-                                                        Path, PathType,
-                                                        Session, SessionStatus,
-                                                        SessionType,
-                                                        TransitionType,
-                                                        UnloadType, Vertex)
-from pydofus2.com.ankamagames.dofus.kernel.net.DisconnectionReasonEnum import \
-    DisconnectionReasonEnum
+from pyd2bot.thriftServer.pyd2botService.ttypes import (
+    JobFilter,
+    Path,
+    PathType,
+    Session,
+    SessionStatus,
+    SessionType,
+    TransitionType,
+    UnloadType,
+    Vertex,
+)
+from pydofus2.com.ankamagames.dofus.kernel.net.DisconnectionReasonEnum import DisconnectionReasonEnum
 from pydofus2.com.ankamagames.dofus.logic.game.common.managers.InventoryManager import InventoryManager
 from pydofus2.com.ankamagames.dofus.logic.game.common.managers.PlayedCharacterManager import PlayedCharacterManager
 from pydofus2.com.ankamagames.jerakine.benchmark.BenchmarkTimer import BenchmarkTimer
 from pydofus2.com.ankamagames.jerakine.logger.Logger import HtmlFormatter, Logger
 import signal
 
+
 def signal_handler(sig, frame):
-    print('You pressed Ctrl+C or the server is reloading!')
+    print("You pressed Ctrl+C or the server is reloading!")
     # Insert your cleanup code here
     for bot_data in BotManagerApp._running_bots.values():
         bot = bot_data["obj"]
@@ -29,11 +34,13 @@ def signal_handler(sig, frame):
         bot.join()
     sys.exit(0)
 
+
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
+
 class SocketIOHandler(logging.Handler):
-    def __init__(self, socketio: 'SocketIO', batch_time=1, *args, **kwargs):
+    def __init__(self, socketio: "SocketIO", batch_time=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.socketio = socketio
         self.batch_time = batch_time
@@ -48,10 +55,11 @@ class SocketIOHandler(logging.Handler):
             self.timer.start()
 
     def flush(self):
-        self.socketio.emit('log_message_batch', self.batch)
+        self.socketio.emit("log_message_batch", self.batch)
         self.batch.clear()
         self.timer = None
-        
+
+
 def format_runtime(startTime):
     seconds = time.time() - startTime
     hours = seconds // 3600
@@ -66,9 +74,10 @@ def format_runtime(startTime):
         result = f"{int(hours)}h {int(minutes)}m {int(seconds)}s"
     return result
 
+
 class BotManagerApp:
     _running_bots = dict()
-    
+
     def __init__(self):
         self.app = Flask(__name__)
         self.socketio = SocketIO(self.app)
@@ -105,7 +114,7 @@ class BotManagerApp:
             bot = bot_data["obj"]
             bot.shutdown(DisconnectionReasonEnum.WANTED_SHUTDOWN, "Brutal shutdown because of app reload")
         self.socketio.stop()
-    
+
     def setup_routes(self):
         @self.app.route("/")
         def index():
@@ -113,17 +122,13 @@ class BotManagerApp:
                 "index.html", accounts=AccountManager.get_accounts(), runningBots=self._running_bots.values()
             )
 
-        @self.app.route('/fight-paths', methods=['GET'])
+        @self.app.route("/fight-paths", methods=["GET"])
         def get_fight_paths():
-            paths = {
-                "Ankarnam (lvl1)": 154010883,
-                "Ankarnam (lvl5)": 154010884,
-                "Astrub village": 191106048
-            }
+            paths = {"Ankarnam (lvl1)": 154010883, "Ankarnam (lvl5)": 154010884, "Astrub village": 191106048}
             # You might want to fetch these values from a database or another service in a real application
             return jsonify({"paths": paths})
 
-        @self.app.route('/solo-fight', methods=['POST'])
+        @self.app.route("/solo-fight", methods=["POST"])
         def solo_fight():
             if not request.is_json:
                 return jsonify({"error": "Request must be JSON"}), 400
@@ -131,14 +136,13 @@ class BotManagerApp:
             data = request.get_json()
 
             # Extracting the necessary information from the request
-            account_id = data.get('account_id')
-            character_id = data.get('character_id')
-            path_value = data.get('path_value')
+            account_id = data.get("account_id")
+            character_id = data.get("character_id")
+            path_value = data.get("path_value")
             try:
-                monster_lvl_coef_diff = float(data.get('monsterLvlCoefDiff'))  # Explicitly convert to float
+                monster_lvl_coef_diff = float(data.get("monsterLvlCoefDiff"))  # Explicitly convert to float
             except ValueError:
                 return jsonify({"error": "Invalid format for monsterLvlCoefDiff. A float is required."}), 400
-
 
             # Placeholder for your logic to process the solo fight
             session = self.get_basic_session(account_id, character_id)
@@ -151,18 +155,20 @@ class BotManagerApp:
             )
             session.monsterLvlCoefDiff = monster_lvl_coef_diff
             # For example, validate the input and then update the database or process the game logic
-            print(f"Processing solo fight for account {account_id}, character {character_id}, on path {path_value} with monster level coefficient difficulty {monster_lvl_coef_diff}")
+            print(
+                f"Processing solo fight for account {account_id}, character {character_id}, on path {path_value} with monster level coefficient difficulty {monster_lvl_coef_diff}"
+            )
 
             self.start_bot_session(session, "solo-fight")
 
             # Responding with a success message or any other relevant information
             return jsonify({"message": "Solo fight initiated successfully"}), 200
-            
+
         @self.app.route("/run/<account_id>/<character_id>/<action>")
         def run_action(account_id, character_id, action):
             try:
                 session = self.get_basic_session(account_id, character_id)
-                
+
                 if action == "treasurehunt":
                     session.type = SessionType.TREASURE_HUNT
 
@@ -172,14 +178,10 @@ class BotManagerApp:
                         Path(
                             id="mine_astrub",
                             type=PathType.CustomRandomFarmPath,
-                            mapIds=[ 193331715, 193200131, 188484108 ]
+                            mapIds=[193331715, 193200131, 188484108],
                         ),
-                        Path(
-                            id="mine_astrub2",
-                            type=PathType.CustomRandomFarmPath,
-                            mapIds=[ 193331713, 193200129 ]
-                        ),
-                    ],
+                        Path(id="mine_astrub2", type=PathType.CustomRandomFarmPath, mapIds=[193331713, 193200129]),
+                    ]
                     session.jobFilters = [
                         JobFilter(36, []),  # Pêcheur goujon
                         JobFilter(2, []),  # Bucheron,
@@ -188,15 +190,15 @@ class BotManagerApp:
                         JobFilter(1, [311]),  # Base : eau
                         JobFilter(24, []),  # Miner
                     ]
-                    
+
                 else:
                     return jsonify({"status": "error", "message": f"Unknown action {action}"})
-                
+
                 self.start_bot_session(session, action)
-                
+
             except Exception as e:
                 return jsonify({"status": "error", "message": f"Error while running {action} : {e}"})
-            
+
             return jsonify(
                 {
                     "status": "success",
@@ -234,17 +236,16 @@ class BotManagerApp:
                 result.append(bot_data)
             return jsonify(result)
 
-        @self.app.route("/watch-log", methods=['POST'])
+        @self.app.route("/watch-log", methods=["POST"])
         def watch_log():
             data = request.json
-            action = data['action']
-            
+            action = data["action"]
+
             if action == "start":
-                bot_name = data['name']
+                bot_name = data["name"]
                 if self._log_handler:
                     return {"error": "Log watching already started"}, 400
-    
-    
+
                 botlogger = Logger.getInstance(bot_name)
                 if not botlogger:
                     return {"error": f"Bot {bot_name} logger instance not found"}, 400
@@ -255,9 +256,9 @@ class BotManagerApp:
                 handler.setFormatter(formatter)
                 botlogger.addHandler(handler)
                 self._log_handler = {"bot": bot_name, "obj": handler}
-                
+
                 return {"message": "Log watching started"}, 200
-            
+
             elif action == "stop":
                 if self._log_handler:
                     botlogger = Logger.getInstance(self._log_handler["bot"])
@@ -266,8 +267,8 @@ class BotManagerApp:
                     return {"message": "Log watching stopped"}, 200
 
             return {"error": "Invalid action or name"}, 400
-        
-        @self.app.route('/import_accounts', methods=['GET'])
+
+        @self.app.route("/import_accounts", methods=["GET"])
         def import_accounts():
             try:
                 AccountManager.clear()

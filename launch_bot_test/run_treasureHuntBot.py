7fba6a2e6fbbc2ad69796c0d032c744dafc045ce
ShadowsFox98 <164229769+ShadowsFox98@users.noreply.github.com>
2024-03-22 12:33:43 +0100

    fix: Add feca to breed config (#13)

--- launch_bot_test/run_treasureHuntBot.py
+++ launch_bot_test/run_treasureHuntBot.py
@@ -12,7 +12,7 @@ from pyd2bot.thriftServer.pyd2botService.ttypes import (Session, SessionType,
 __dir__ = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
-    account_key = "244588168088949842"
+    account_key = "244588168235074572"
     creds = AccountManager.get_credentials(account_key)
     session = Session(
         id="account_key",

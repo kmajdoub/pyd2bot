336c3f4bb2aa043b9c0a0ac1a870dbc63fb5fc27
Hexalgo <hexalgo@gmail.com>
2024-03-25 13:51:19 +0100

    added one extra check for keys vault dir

--- pyd2bot/BotConstants.py
+++ pyd2bot/BotConstants.py
@@ -2,7 +2,11 @@ import os
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
 class BotConstants:
-    PERSISTENCE_DIR = os.path.join(BASEDIR, "persistence")
+    PERSISTENCE_DIR = os.path.join(os.getenv("APPDATA"), "pyd2bot", "persistence")
+    KEYS_DIR = os.path.join(os.getenv("APPDATA"), "pyd2bot", "secrects", "vault")
+    
     if not os.path.exists(PERSISTENCE_DIR):
         os.makedirs(PERSISTENCE_DIR)
-    KEYS_DIR = os.path.join(os.getenv("APPDATA"), "pyd2bot", "secrects", "vault")
+    
+    if not os.path.exists(KEYS_DIR):
+        os.makedirs(KEYS_DIR)
